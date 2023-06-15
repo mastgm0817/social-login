52ebb1a583f6b9fad0755c802ba8fac1826cdc10
rhquddus <rhquddus@naver.com>
2023-06-15 14:03:08 +0900

    dd

--- src/main/java/home/myself/social/login/model/OAuthAttributes.java
+++ src/main/java/home/myself/social/login/model/OAuthAttributes.java
@@ -27,11 +27,11 @@ public class OAuthAttributes {
     // 여기서 registrationId는 OAuth2 로그인을 처리한 서비스 명("google","kakao","naver"..)이 되고,
     // userNameAttributeName은 해당 서비스의 map의 키값이 되는 값이됩니다. {google="sub", kakao="id", naver="response"}
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
-//        if (registrationId.equals("kakao")) {
-//            return ofKakao(userNameAttributeName, attributes);
-//        } else if (registrationId.equals("naver")) {
-//            return ofNaver(userNameAttributeName,attributes);
-//        }
+        if (registrationId.equals("kakao")) {
+            return ofKakao(userNameAttributeName, attributes);
+        } else if (registrationId.equals("naver")) {
+            return ofNaver(userNameAttributeName,attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -41,6 +41,27 @@ public class OAuthAttributes {
                 (String) attributes.get("name"),
                 (String) attributes.get("email"),
                 (String) attributes.get("picture"));
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");  // 카카오로 받은 데이터에서 계정 정보가 담긴 kakao_account 값을 꺼낸다.
+        Map<String, Object> profile = (Map<String, Object>) kakao_account.get("profile");   // 마찬가지로 profile(nickname, image_url.. 등) 정보가 담긴 값을 꺼낸다.
+        System.out.println(profile);
+        return new OAuthAttributes(attributes,
+                userNameAttributeName,
+                (String) profile.get("nickname"),
+                (String) kakao_account.get("email"),
+                (String) profile.get("profile_image"));
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");    // 네이버에서 받은 데이터에서 프로필 정보다 담긴 response 값을 꺼낸다.
+
+        return new OAuthAttributes(attributes,
+                userNameAttributeName,
+                (String) response.get("name"),
+                (String) response.get("email"),
+                (String) response.get("profile_image"));
     }
 
     public User toEntity() {
